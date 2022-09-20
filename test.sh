fa30f2fdc47408a14b66f8d77cf59545d5f314b6
lapsoos <lasvmateo@gmail.com>
2022-09-20 16:28:14 +0200

    20/09/22

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 download(){
     read playlist_link
-    yt-dlp --geo-bypass --yes-playlist --break-on-existing --break-per-input --downloader aria2c --format "bv*+ba/b"  playlist_link
+    yt-dlp --geo-bypass --yes-playlist --break-on-existing --break-per-input --downloader aria2c --format "bv*+ba/b"  $playlist_link
 }
 log(){
     #logs the names of downloaded files to a log file
@@ -15,22 +15,47 @@ log(){
         touch $logfile #create registry
     fi
     echo "logging started"
-    for filename in *.webm; 
+    for filename in *.webm; #loop through all files with .webm extension
     do
-        if [ -f $filename in $logfile]; then
-
-            echo "$filename already logged"
+        if grep -Fxq "$filename" $logfile; #check if file is already in registry
+        then
+            echo "$filename already logged" #if file is already in registry skip
         else
-            echo "logging $filename"
-            echo "$filename" >> $logfile
+            echo "logging $filename" 
+            echo "$filename" >> $logfile #if file is not in registry add to registry
         fi
     done
     echo "logging finished"
 }
+check(){
+    #checks if logged files are still available online
+    #if not adds "OFFLINE - " to the beginning of the file name
+    echo "Enter the logfile name:"
+    read logfile
+    if [ -f $logfile ] ; then #check if registry exists
+        echo "Log found"
+    else 
+        echo "no file found"
+        exit 1
+    fi
+    echo "checking started"
+    while read -r  line; do #loop through all lines in registry
+        if yt-dlp --geo-bypass --break-on-existing --downloader aria2c --format "bv*+ba/b" $line; then #check if file is still available online
+            echo "$line is online" #if file is online skip
+        else
+            echo "$line is offline" #if file is offline rename file
+            mv "$line.webm" "OFFLINE - $line.webm"
+        fi
+    done < $logfile
+    echo "checking finished"
+
+}
+
+
 main(){
     echo "1) Download"
-    echo "2) Log"
-    echo "3) #Check"
+    echo "2) Log new files"
+    echo "3) Check"
     echo "4) #Change Directory"
     echo "5) #Exit"
     read -n 1 ans1
